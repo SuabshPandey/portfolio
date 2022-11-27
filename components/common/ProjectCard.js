fa5f42cfc5caaa66cb68bc3277df788d46b397f3
Subash Pandey <itsmesubashpandey867@gmail.com>
2022-11-27 15:41:00 +0545

    [Updated] Completed project and contact section

---
+++ components/common/ProjectCard.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import React from "react";
+import styles from "./projectcard.module.scss";
+
+const ProjectCard = ({ project }) => {
+  const { name, link, image, view_on, description } = project;
+  return (
+    <>
+      <div className={`${styles.card_main_div} card`}>
+        <div className={`${styles.card_img_div}`}>
+          <img
+            src={image}
+            className={`${styles.card_img} card-img-top`}
+            alt={name}
+          />
+        </div>
+        <div className={`${styles.card_content} card-body`}>
+          <h1 className={`${styles.card_title} card-title`}>
+            {name.substring(0, 27)}
+          </h1>
+          <p className={`${styles.card_content} card-text`}>
+            {" "}
+            {description.substring(0, 105)}
+            {" ... "}
+          </p>
+          <Link href={link} className={styles.view_link} target="_blank">
+            View {view_on}
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProjectCard;
--- pages/projects.js
+++ pages/projects.js
@@ -1,4 +1,6 @@
 import React from "react";
+import ProjectCard from "../components/common/ProjectCard";
+import projectList from "../components/home/projectList";
 import styles from "../styles/project.module.scss";
 
 const Projects = () => {
@@ -12,6 +14,18 @@ const Projects = () => {
           back-end of a project. I currently work as a freelance web developer
           and designer.
         </p>
+        <div className={`${styles.project_row} row`}>
+          {projectList.map((project, index) => {
+            return (
+              <div
+                className={`${styles.project_col} col-lg-4 col-md-4 col-sm-6`}
+                key={index}
+              >
+                <ProjectCard project={project} />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
